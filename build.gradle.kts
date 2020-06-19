c107f48ba631acebca1bbdf9bc155e6c1ee9e889
NamuTree0345 <johanbag290@gmail.com>
2020-06-19 12:51:10 +0900

    First commit

---
+++ build.gradle.kts
@@ -0,0 +1,46 @@
+import kr.entree.spigradle.kotlin.spigot
+
+plugins {
+    kotlin("jvm") version "1.3.72"
+    id("kr.entree.spigradle") version "1.2.2"
+}
+
+group = "xyz.gwcr.clearsky"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven("https://maven.heartpattern.kr/repository/maven-public/") //HeartPattern’s MC maven repo
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+
+    compileOnly(spigot("1.15.2")) // spigot api
+}
+
+spigot { // plugin.yml 설정
+    authors = listOf("NamuTree0345")
+}
+
+val shade = configurations.create("shade")
+shade.extendsFrom(configurations.implementation.get())
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    jar {
+        from(
+            shade.map {
+                if (it.isDirectory)
+                    it
+                else
+                    zipTree(it)
+            }
+        )
+    }
+}
